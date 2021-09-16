945c1366d50387aac59334a1f04bdb9bb2414a17
DIR\giuseppe.notaro <gnotaro23@gmail.com>
2021-09-16 17:13:41 +0200

    fix result0

--- src/pages/projectPage/search/Result0.tsx
+++ src/pages/projectPage/search/Result0.tsx
@@ -4,7 +4,7 @@ import {faTree} from "@fortawesome/free-solid-svg-icons";
 const Result0 = () =>{
     return(
         <div >
-            <p style={{ fontSize:"12px", fontWeight:"bold",paddingLeft:"9%"}}>Risultati : 0 </p>
+            <p style={{ fontSize:"12px", fontWeight:"bold",paddingLeft:"9%",marginBottom:"6%"}}>Risultati : 0 </p>
             <FontAwesomeIcon style={{textAlign:"center", width:"100%"}} size="10x" icon={faTree}/>
             <p style={{textAlign:"center",width:"100%" ,fontSize:"12px", fontWeight:"bold", marginTop:"5%"}}>Non ci sono risultati</p>
             <p style={{textAlign:"center",width:"100%" ,fontSize:"10px", fontWeight:"bold",paddingBottom:"10%"}}> Applica i filtri per iniziare una ricerca</p>
